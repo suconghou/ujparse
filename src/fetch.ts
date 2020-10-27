57ef5f61646d42b586e51a61e4fde0dffe153fb2
suconghou <1126045770@qq.com>
2020-10-27 10:32:54 +0800

    more cache logic in case of cf not cached

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,7 +1,38 @@
 
 // 当前的http执行器是cf worker的fetch
 // 可以改写成基于xhr或node http request都可以
+
+const cache = new Map()
+
+const get = (key: string) => {
+    const item = cache.get(key)
+    if (item) {
+        if (item.expire > +new Date()) {
+            return item.value
+        } else {
+            expire()
+        }
+    }
+}
+
+const set = (key: string, value: any, ttl: number = 3600e3) => {
+    cache.set(key, { value, expire: +new Date() + ttl })
+}
+
+const expire = () => {
+    const t = +new Date()
+    for (let [k, v] of cache) {
+        if (v.expire < t) {
+            cache.delete(k)
+        }
+    }
+}
+
 export const ajax = async (url: string): Promise<string> => {
+    let text = get(url)
+    if (text) {
+        return text
+    }
     const headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:74.0) Gecko/20100101 Firefox/74.0' }
     const init = {
         headers,
@@ -13,5 +44,7 @@ export const ajax = async (url: string): Promise<string> => {
         }
     } as any
     const r = await fetch(url, init)
-    return await r.text()
+    text = await r.text()
+    set(url, text)
+    return text
 }
